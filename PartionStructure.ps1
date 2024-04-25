7cd42f99f22ad8c7b0ec080d62ee8a61a47d1378
PrashanthaTS <168079022+PrashanthaTS@users.noreply.github.com>
2024-04-25 19:23:44 +0530

    Add files via upload

---
+++ PartionStructure.ps1
@@ -0,0 +1,37 @@
+﻿Set-ExecutionPolicy Unrestricted
+$partitionSet = New-Object System.Collections.Generic.HashSet[string]
+
+# Get information about all partitions on the specified disk
+$diskNumber = 0  # Change this to the appropriate disk number
+$partitions = Get-Partition -DiskNumber $diskNumber
+
+
+
+# Loop through each partition
+foreach ($partition in $partitions) {
+    #$partitionNumber = $partition.PartitionNumber
+    #$type = $partition.Type
+    #$driveLetter=$partition.DriveLetter
+    $partitionSet.Add($partition.type)  
+     
+
+}
+    Write-Host 'Actual Partition--->'  $partitionSet
+
+$ExpectedPartitionSet =New-Object System.Collections.Generic.HashSet[string]
+$ExpectedPartitionSet = ('System' ,'MSR' ,'Windows' ,'Reserved')
+
+    Write-Host 'Expected Partition--->'$ExpectedPartitionSet
+
+if ($partitionSet -eq $ExpectedPartitionSet)
+{
+    Write-host 'Partirion Struture is proper'
+}
+else
+{
+ Write-host 'Partition structure is not as expected'
+}
+
+
+
+
